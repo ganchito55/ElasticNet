fd44cd93267d75a19611cea9686683cf0c20a3a6
Jorge Durán <ganchito55@gmail.com>
2017-01-10 12:17:14 +0100

    Add project files.

---
+++ ElasticNet/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Nest;
+using System;
+
+namespace ElasticNet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var node = new Uri("http://elastic:mineoMineo@188.166.147.155:4444");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            var response = client.CatIndices();
+        }
+    }
+}
